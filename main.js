b41774e9a3988745f8251da77ee9e02116d1ca53
Wes <wes.coffay@gmail.com>
2017-07-21 18:11:54 -0400

    fixed error for y axis

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const canvasDiv = document.getElementById('canvasDiv');
 canvas = document.createElement('canvas');
-canvas.setAttribute('width', 200);
-canvas.setAttribute('height', 200);
+canvas.setAttribute('width', 1000);
+canvas.setAttribute('height', 1000);
 canvas.setAttribute('id', 'canvas');
 canvasDiv.appendChild(canvas);
 if(typeof G_vmlCanvasManager != 'undefined') {
@@ -9,54 +9,61 @@ if(typeof G_vmlCanvasManager != 'undefined') {
 }
 ctx = canvas.getContext("2d");
 
+const colorPurple = "#cb3594";
+const colorGreen = "#659b41";
+const colorYellow = "#ffcf33";
+const colorBrown = "#986928";
 
-    //code here
-    $('#canvas').mousedown(function(e) {
-      let mouseX = e.pageX - this.offsetLeft
-      let mouseY = e.pageY - this.offsetTop
+let curColor= colorGreen
+const clickColor = new Array()
 
-      paint = true
-      addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop)
-      redraw()
-    })
-    $('#canvas').mousemove(function(e) {
-      if (paint) {
-        addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true)
-        redraw()
-      }
-    })
-    $('#canvas').mouseup(function(e) {
-      paint = false
-    })
-    $('#canvas').mouseleave(function(e) {
-      paint = false
-    })
-    let clickX = new Array()
-    let clickY = new Array()
-    let clickDrag = new Array()
-    let paint
+$('#canvas').mousedown(function(e) {
+  let mouseX = e.pageX - this.offsetLeft
+  let mouseY = e.pageY - this.offsetTop
 
-    function addClick(x, y, dragging) {
-      clickX.push(x)
-      clickY.push(y)
-      clickDrag.push(dragging)
+  paint = true
+  addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop)
+  redraw()
+})
+$('#canvas').mousemove(function(e) {
+  if (paint) {
+    addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true)
+    redraw()
+  }
+})
+$('#canvas').mouseup(function(e) {
+  paint = false
+})
+$('#canvas').mouseleave(function(e) {
+  paint = false
+})
+let clickX = new Array()
+let clickY = new Array()
+let clickDrag = new Array()
+let paint
+
+function addClick(x, y, dragging) {
+  clickX.push(x)
+  clickY.push(y)
+  clickDrag.push(dragging)
+  clickColor.push(curColor)
+}
+
+function redraw() {
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height) //clears canvas
+  ctx.linejoin = 'round'
+  ctx.lineWidth = 5
+
+  for (let i=0; i<clickX.length; i++) {
+    ctx.beginPath()
+    if(clickDrag[i] && i){
+      ctx.moveTo(clickX[i-1], clickY[i-1])
+    }else{
+      ctx.moveTo(clickX[i]-1, clickY[i])
     }
-
-    function redraw() {
-      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height) //clears canvas
-      ctx.strokeStyle = "#df4b26"
-      ctx.linejoin = 'round'
-      ctx.lineWidth = 5
-
-      for (let i=0; i<clickX.length; i++) {
-        ctx.beginPath()
-        if(clickDrag[i] && i){
-          ctx.moveTo(clickX[i-1], clickY[i-1])
-        }else{
-          ctx.moveTo(clickX[i]-1, clickY[1])
-        }
-        ctx.lineTo(clickX[i], clickY[i])
-        ctx.closePath()
-        ctx.stroke()
-      }
-    }
+    ctx.lineTo(clickX[i], clickY[i])
+    ctx.closePath()
+    ctx.strokeStyle = clickColor[i]
+    ctx.stroke()
+  }
+}
