54818f5009e8eebedc5121a723414c805b174334
Wes <wes.coffay@gmail.com>
2017-07-21 17:57:30 -0400

    basic functionality works

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-function draw() {
-  const canvas = document.getElementById('tutorial')
-  if (canvas.getContext) {
-    const ctx = canvas.getContext('2d')
+const canvasDiv = document.getElementById('canvasDiv');
+canvas = document.createElement('canvas');
+canvas.setAttribute('width', 200);
+canvas.setAttribute('height', 200);
+canvas.setAttribute('id', 'canvas');
+canvasDiv.appendChild(canvas);
+if(typeof G_vmlCanvasManager != 'undefined') {
+	canvas = G_vmlCanvasManager.initElement(canvas);
+}
+ctx = canvas.getContext("2d");
+
+
     //code here
-    canvas.mousedown(function(e) {
+    $('#canvas').mousedown(function(e) {
       let mouseX = e.pageX - this.offsetLeft
       let mouseY = e.pageY - this.offsetTop
 
@@ -11,18 +19,44 @@ function draw() {
       addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop)
       redraw()
     })
-    canvas.mousemove(function(e) {
+    $('#canvas').mousemove(function(e) {
       if (paint) {
-        addClick(e.pageX = this.offsetLeft, e.pageY - this.offsetTop, true)
+        addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true)
         redraw()
       }
     })
-    canvas.mouseup(function(e) {
+    $('#canvas').mouseup(function(e) {
       paint = false
     })
-    
-  } else {
-    //unsupported code
-    console.log('canvas not supported');
-  }
-}
+    $('#canvas').mouseleave(function(e) {
+      paint = false
+    })
+    let clickX = new Array()
+    let clickY = new Array()
+    let clickDrag = new Array()
+    let paint
+
+    function addClick(x, y, dragging) {
+      clickX.push(x)
+      clickY.push(y)
+      clickDrag.push(dragging)
+    }
+
+    function redraw() {
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height) //clears canvas
+      ctx.strokeStyle = "#df4b26"
+      ctx.linejoin = 'round'
+      ctx.lineWidth = 5
+
+      for (let i=0; i<clickX.length; i++) {
+        ctx.beginPath()
+        if(clickDrag[i] && i){
+          ctx.moveTo(clickX[i-1], clickY[i-1])
+        }else{
+          ctx.moveTo(clickX[i]-1, clickY[1])
+        }
+        ctx.lineTo(clickX[i], clickY[i])
+        ctx.closePath()
+        ctx.stroke()
+      }
+    }
